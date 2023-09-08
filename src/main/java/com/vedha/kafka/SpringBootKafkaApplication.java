055fddea5e9c28cebc5a8fa121140ca7a62887af
Sarath P <sarath1242000@gmail.com>
2023-09-08 11:33:27 +0530

    Spring Boot Kafka Demo

--- src/main/java/com/vedha/kafka/SpringBootKafkaApplication.java
+++ src/main/java/com/vedha/kafka/SpringBootKafkaApplication.java
@@ -1,9 +1,12 @@
 package com.vedha.kafka;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Spring Boot Kafka App",  description = "Spring Boot Kafka Demo"))
 public class SpringBootKafkaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/vedha/kafka/controller/MessageController.java
+++ src/main/java/com/vedha/kafka/controller/MessageController.java
@@ -3,6 +3,9 @@ package com.vedha.kafka.controller;
 import com.vedha.kafka.dto.UserDTO;
 import com.vedha.kafka.producer.KafkaJsonProducer;
 import com.vedha.kafka.producer.KafkaProducer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/kafka")
 @AllArgsConstructor
+@Tag(name = "Message Controller", description = "Kafka Message Controller")
 public class MessageController {
 
     private final KafkaProducer kafkaProducer;
@@ -18,6 +22,8 @@ public class MessageController {
     private final KafkaJsonProducer kafkaJsonProducer;
 
     // http://localhost:8080/api/kafka/sendMessage?message=Hi Vedha
+    @Operation(summary = "Send String Message", description = "Send String Message To Kafka")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 Ok")
     @GetMapping(value = { "/sendMessage" })
     public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
 
@@ -27,6 +33,8 @@ public class MessageController {
     }
 
     // http://localhost:8080/api/kafka/sendJsonMessage
+    @Operation(summary = "Send Json Message", description = "Send Json Message To Kafka")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 Ok")
     @PostMapping(value = { "sendJsonMessage" }, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = { MediaType.TEXT_PLAIN_VALUE })
     public ResponseEntity<String> sendJsonMessage(@RequestBody UserDTO userDTO) {
 
